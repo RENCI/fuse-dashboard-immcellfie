2bcf7ec76bb67a151064ccfe25a786cb846e4b06
David <borland@renci.org>
2021-01-15 16:22:06 -0500

    Hello world

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+export const App = () => { 
+  return (
+    <Container>
+      <h3>Hello World</h3>
+    </Container>
+  ); 
+};
