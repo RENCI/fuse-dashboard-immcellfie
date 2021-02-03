74fc24be3f5a1e7150cb523fadb80913b853d39f
David <borland@renci.org>
2021-02-02 22:45:49 -0500

    Add vega-lite heatmap of input gene expression data

--- src/views/input-view.js
+++ src/views/input-view.js
@@ -1,12 +1,69 @@
 import React, { useContext } from "react";
 import { DataContext } from "../contexts";
+import { VegaWrapper } from "../components/vega-wrapper";
+
+const spec = {
+  $schema: "https://vega.github.io/schema/vega-lite/v4.json",
+  width: "container",
+  height: "container",
+  autosize: {
+    resize: true
+  },
+  data: {
+    name: "data"
+  },
+  mark: { 
+    type: "rect",
+    tooltip: true
+  },
+  encoding: {
+    y: {
+      field: "id", 
+      type: "ordinal",
+      sort: {
+        op: "mean",
+        field: "value",
+        order: "descending"
+      }
+    },
+    x: {
+      field: "gene", 
+      type: "ordinal"
+    },
+    color: { 
+      field: "value",
+      type: "quantitative",
+      scale: {
+        type: "symlog"
+      }
+    }
+  }
+};
 
 export const InputView = () => {
   const [data] = useContext(DataContext);
 
   const { input } = data;
 
+  // Transform input to work with heatmap
+  const heatmapData = !input ? [] : input.data.reduce((p, c) => {
+    return p.concat(c.values.map((d, i) => {
+      return {
+        id: c.id,
+        gene: i,
+        value: d
+      };
+    }));
+  }, []);
+
   return (
-    <pre>{ input ? JSON.stringify(input, null, 2) : "No input" }</pre>
+    <>
+      { input ? 
+        <>
+          <VegaWrapper spec={ spec } data={ heatmapData } height={ input.data.length * 10 + "px" } />
+        </>
+      : <h4>No input</h4>
+      }
+    </>
   ); 
 };
