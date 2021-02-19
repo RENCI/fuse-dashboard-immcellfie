17de21c3729ea55666008cf6c820ad3b587a15a1
David <borland@renci.org>
2021-02-18 19:33:26 -0500

    Clean up escher code

--- src/components/escher-wrapper/escher-wrapper.js
+++ src/components/escher-wrapper/escher-wrapper.js
@@ -1,58 +1,39 @@
 import React, { useRef, useEffect } from "react";
-import Builder, { libs } from "escher";
+import Builder from "escher";
+import { json } from "d3-fetch";
 import "./escher-wrapper.css";
-
-const d3Json = libs.d3_json;
 
 export const EscherWrapper = () => {
   const div = useRef();
-
-  const getJSON = url => {
-    return new Promise((resolve, reject) => {
-      d3Json(url, (e, d) => {
-        if (e) reject(e);
-        else resolve(d);
-      });
-    })
-  };
   
   const draw = () => {
-    // Load a JSON file for the map from the network
-    Promise.all([
-      getJSON('/data/escher/iJO1366.json'),
-    ]).then(([model]) => {
-      // ---------------------------------------
-      // First map: Just show the map
-      // ---------------------------------------
-  
+    json("/data/escher/iJO1366.json").then(model => {  
       const options1 = {
-        /* just show the zoom buttons */
         menu: 'all',
-        // use the smooth pan and zoom option
         use_3d_transform: true,
-        /* no editing in this map */
         enable_editing: true,
-        /* no keyboard shortcuts */
         enable_keys: true,
+        first_load_callback,
         fill_screen: false,
         never_ask_before_quit: true,
         show_gene_reaction_rules: false,
         full_screen_button: true,
-        first_load_callback,
         reaction_data: { PPS: 20, THZPSN3: 20 },
         metabolite_data: { atp_c: 20 },
         // reaction_no_data_size: 25,
         // metabolite_no_data_size: 25,
         canvas_size_and_loc: {
-          x: 0,
-          y: 0,
-          width: 500,
-          height: 500
+          x: -900,
+          y: -900,
+          width: 2000,
+          height: 2000
         }
       };
   
       Builder(null, model, null, div.current, options1)
-    }, log)
+    }).catch(error => {
+      console.log(error);
+    });
   }
 
   const first_load_callback = builder => {
@@ -74,6 +55,7 @@ export const EscherWrapper = () => {
     draw('XYLabcpp', { x: -200, y: -200 }, -135);
     draw('THZPSN3', { x: 600, y: 200 }, 45);
     draw('PPA2', { x: -200, y: 200 }, 135);
+
     builder.map.new_reaction_from_scratch(
       'Ec_biomass_iJO1366_WT_53p95M',
       { x: 0, y: 1300 }, 
@@ -86,10 +68,6 @@ export const EscherWrapper = () => {
     // After building a reaction, Escher selects the newest
     // metabolite. Unselect it like this.
     builder.map.select_none();
-  };
-
-  const log = e => {
-    console.log(e);
   };
 
   useEffect(() => {
