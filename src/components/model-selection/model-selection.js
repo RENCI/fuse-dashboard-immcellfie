d466ae3da45986bb972b405bac54be80e17a38d2
David <borland@renci.org>
2021-05-06 17:10:28 -0400

    Work on using correct parameters for CellFIE

--- src/components/model-selection/model-selection.js
+++ src/components/model-selection/model-selection.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useReducer, useContext } from "react";
 import { Row, Col, Card, Form } from "react-bootstrap";
 import { SpinnerButton } from "../spinner-button";
 import { DataContext } from "../../contexts";
@@ -7,23 +7,44 @@ import { api } from "../../api";
 const { Title, Body } = Card;
 const { Label, Group, Control, Switch } = Form;
 
-const organisms = [
-  "human",
-  "mouse",
-  "rat",
-  "Chinese hamster"
+const models = [
+  { organism: "human", name: "iHSA", value: "MT_iHsa.mat" },
+  { organism: "human", name: "recon v1", value: "MT_recon_1.mat" },
+  { organism: "human", name: "recon v2", value: "MT_recon_2.mat" },
+  { organism: "human", name: "recon v2.2", value: "MT_recon_2_2_entrez.mat" },
+  { organism: "mouse", name: "iMM1416", value: "MT_iMM1415.mat" },
+//  { organism: "mouse", name: "inesMouseModel" },
+  { organism: "mouse", name: "quek", value: "MT_quek14.mat" },
+  { organism: "rat", name: "iRno", value: "MT_iRno.mat" },
+  { organism: "Chinese hamster", name: "iCHOv1", value: "MT_iCHOv1_final.mat" }
 ];
 
-const models = [
-  { organism: "human", name: "iHSA" },
-  { organism: "human", name: "recon_v1" },
-  { organism: "human", name: "recon_v2" },
-  { organism: "human", name: "recon_v2.2" },
-  { organism: "Chinese hamster", name: "iCHOv1" },
-  { organism: "mouse", name: "iMM1416" },
-  { organism: "mouse", name: "inesMouseModel" },
-  { organism: "mouse", name: "quek" },
-  { organism: "rat", name: "iRno" }
+const thresholdTypes = [
+  { name: "global", value: "global-thresh" },
+  { name: "local", value: "local-thresh" }
+];
+
+const initialParameters = [
+  {
+    name: "Percentile or value",
+    default: "percentile",
+    value: "percentile",
+    options: ["value", "percentile"]
+  },
+  {
+    name: "Percentile",
+    type: "global",
+    default: 50,
+    value: 50,
+    range: [0, 100]
+  },
+  {
+    name: "Value",
+    type: "global",
+    default: 5,
+    value: 5,
+    range: [0, Number.MAX_SAFE_INTEGER]
+  }
 ];
 
 export const ModelSelection = ({ outputName, outputType }) => {
@@ -31,8 +52,31 @@ export const ModelSelection = ({ outputName, outputType }) => {
   const [organism, setOrganism] = useState("human");
   const [currentModels, setCurrentModels] = useState(models.filter(({ organism }) => organism === "human"));
   const [model, setModel] = useState(models.find(({ organism }) => organism === "human"));
+  const [thresholdType, setThresholdType] = useState(thresholdTypes[0]);
   const [running, setRunning] = useState(false);
+  const [parameters, dispatch] = useReducer((state, action) => {
+    switch (action.type) {
+      case "setValue":  {
+        const newState = [...state];
+
+        const parameter = newState.find(({ name }) => name === action.name);
+
+        parameter.value = action.value;
+
+        return newState;
+      }
+
+      default:
+        throw new Error("Invalid parameters action: " + action.type);
+    }
+  }, initialParameters);
   //const [message, setMessage] = useState();
+
+  const organisms = models.reduce((organisms, model) => {
+    if (!organisms.includes(model.organism)) organisms.push(model.organism);
+
+    return organisms;
+  }, []);
 
   const onOrganismChange = evt => {
     const value = evt.target.value;
@@ -44,8 +88,16 @@ export const ModelSelection = ({ outputName, outputType }) => {
   };
 
   const onModelChange = evt => {
-    setModel(models.find(({ name }) => name === evt.target.value));
-  }
+    setModel(models.find(({ value }) => value === evt.target.value));
+  };
+
+  const onThresholdTypeChange = evt => {
+    setThresholdType(thresholdTypes.find(({ value }) => value === evt.target.value));
+  };
+
+  const onParameterChange = (name, value) => {
+    dispatch({ type: "setValue", name: name, value: value });
+  };
 
   const onRunCellfieClick = () => {
     setRunning(true);
@@ -59,6 +111,47 @@ export const ModelSelection = ({ outputName, outputType }) => {
 //      setMessage("CellFIE output data loaded");
     }, 1000);
   };
+
+  console.log(parameters);
+  console.log(thresholdType);
+
+  const currentParameters = parameters.filter(({ type, name }) => {
+    const lowName = name.toLowerCase();
+    const pv = lowName === "percentile" || lowName === "value";
+
+    return (!type || type === thresholdType.name) && 
+      (!pv || lowName === parameters.find(({ name }) => name === "Percentile or value").value);
+  }).map((parameter, i) => {
+      return (
+        parameter.options ?
+          <Group key={ i } controlId={ parameter.name + "_select" }>
+            <Label><small>{ parameter.name }</small></Label>
+            <Control 
+              as="select"
+              value={ parameter.value }
+              onChange={ evt => onParameterChange(parameter.name, evt.target.value) }
+            >
+              { parameter.options.map((value, i) => (
+                <option key={ i }>{ value }</option>
+              ))}
+            </Control>
+          </Group>
+        : parameter.range ?
+          <Group key={ i } controlId={ parameter.name + "_number" }>
+            <Label><small>{ parameter.name }</small></Label>
+            <Control 
+              as="input"
+              type="number"
+              size="sm"
+              min={ parameter.range[0] }
+              max={ parameter.range[1] }
+              value={ parameter.value }
+              onChange={ evt => onParameterChange(parameter.name, +evt.target.value) }
+            />
+          </Group>
+        : null 
+      );
+    });
 
   return (
     <Card>
@@ -82,40 +175,30 @@ export const ModelSelection = ({ outputName, outputType }) => {
             <Label><h6>Model</h6></Label>
               <Control 
                 as="select"
-                value={ model.name }
+                value={ model.value }
                 onChange={ onModelChange }
               >
-                { currentModels.map(({ name }, i) => (
-                  <option key={ i }>{ name }</option>
+                { currentModels.map(({ name, value }, i) => (
+                  <option key={ i } value={ value }>{ name }</option>
+                ))}
+              </Control>
+            </Group>
+            <Group controlId="threshold_type_select">
+            <Label><h6>Threshold type</h6></Label>
+              <Control 
+                as="select"
+                value={ thresholdType.value }
+                onChange={ onThresholdTypeChange }
+              >
+                { thresholdTypes.map(({ name, value }, i) => (
+                  <option key={ i } value={ value }>{ name }</option>
                 ))}
               </Control>
             </Group>
           </Col>
           <Col>
-            <Label><h6>Model parameters</h6></Label>
-            <Group controlId="parameter1">
-              <Label><small>Parameter 1</small></Label>
-              <Control 
-                as="select"
-                size="sm"
-              >
-                <option>Option 1</option>
-                <option>Option 2</option> 
-              </Control>
-            </Group>
-            <Group controlId="parameter2">
-              <Label><small>Parameter 2</small></Label>
-              <Control 
-                as="input"
-                type="number"
-                size="sm"
-                defaultValue="1"
-              />
-            </Group>
-            <Group controlId="parameter3">
-              <Label><small>Parameter 3</small></Label>
-              <Switch  />
-            </Group>
+            <Label><h6><span className="text-capitalize">{ thresholdType.name }</span> parameters</h6></Label>
+            { currentParameters }
           </Col>
         </Row>
         <Row>
