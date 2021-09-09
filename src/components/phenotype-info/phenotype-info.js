45c05cab2cc9b6b759d0c3697e32777e6a202aaa
David <borland@renci.org>
2021-09-09 14:52:58 -0400

    Adjust expression and phenotype data info

--- src/components/phenotype-info/phenotype-info.js
+++ src/components/phenotype-info/phenotype-info.js
@@ -5,14 +5,22 @@ import "./phenotype-info.css";
 export const PhenotypeInfo = ({ source, name, data }) => {
   return (
     <Alert variant="info" className="phenotypeInfo">
-      <u>Phenotype data loaded</u>
-      <div><small>Source: { source }</small></div>
-      <div><small>Name: { name }</small></div>
-      <div><small>{ data.length.toLocaleString() } rows (subjects)</small></div>
-      <div><small>{ data.columns.length.toLocaleString() } columns (dimensions)</small></div>
-      <div className="ml-3 text-muted">
-        <small>{ data.columns.join(", ") }</small>
-      </div>
+      { data ? 
+        <>
+          <u>Phenotype data loaded</u>
+          <div><small>Source: { source }</small></div>
+          <div><small>Name: { name }</small></div>
+          <div><small>{ data.length.toLocaleString() } rows (subjects)</small></div>
+          <div><small>{ data.columns.length.toLocaleString() } columns (dimensions)</small></div>
+          <div className="ml-3 text-muted">
+            <small>{ data.columns.join(", ") }</small>
+          </div>
+        </>
+      :
+        <>
+          No phenotype data
+        </>
+      }
     </Alert>
   );
 };           
