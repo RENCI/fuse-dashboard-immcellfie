80872379209b78bbf6aac11d402a3c9b023ced4a
David <borland@renci.org>
2021-09-17 16:28:10 -0400

    Remove regular expression warning

--- src/components/user-input/user-input.js
+++ src/components/user-input/user-input.js
@@ -11,7 +11,7 @@ export const UserInput = () => {
   const [emailValid, setEmailValid] = useState(false);
 
   const validateEmail = email => {
-    const regex = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;      
+    const regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;      
 
     return regex.test(email);
   };
