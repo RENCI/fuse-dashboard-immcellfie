106a97cfba9256d74ca2f188b0d249143693fa4c
David <borland@renci.org>
2021-10-13 10:07:30 -0400

    Add HomeLink to input view if no email

--- src/views/input-view.js
+++ src/views/input-view.js
@@ -3,6 +3,7 @@ import { UserContext } from "../contexts";
 import { ViewWrapper } from "../components/view-wrapper";
 import { DataSelection } from "../components/data-selection";
 import { DataMissing } from "../components/data-missing";
+import { HomeLink } from "../components/page-links";
 
 export const InputView = () => {
   const [{ email }] = useContext(UserContext);
@@ -11,7 +12,7 @@ export const InputView = () => {
     <>
       { !email ?
         <ViewWrapper>
-          <DataMissing message="No user email selected" showHome={ true } />
+          <DataMissing message="No user email selected" pageLink={ <HomeLink /> } />
         </ViewWrapper>
       :
         <ViewWrapper>
