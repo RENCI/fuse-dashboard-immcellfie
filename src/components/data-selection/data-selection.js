bbbbbb7d00b1c106ac533c2e132eb3ca68101e0e
David <borland@renci.org>
2022-01-12 10:39:58 -0500

    Remove console.log statement

--- src/components/data-selection/data-selection.js
+++ src/components/data-selection/data-selection.js
@@ -23,8 +23,6 @@ export const DataSelection = () => {
 
   useEffect(() => {
     if (dataInfo && dataInfo.source.name === "upload" && dataInfo.phenotypes.numSamples !== dataInfo.expression.numSamples) {
-      console.log(dataInfo);
-
       setErrorMessage(`Number of samples in phenotype data (${ dataInfo.phenotypes.numSamples }) does not match number of subjects in expression data (${ dataInfo.expression.numSamples }). Please upload data with matching sample numbers.`);
     }
     else {
